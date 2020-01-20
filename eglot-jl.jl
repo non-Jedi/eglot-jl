039e20890131804f09279d961e8737beda867908
Adam Beckmeyer <adam_gpg@thebeckmeyers.xyz>
2020-01-20 09:57:13 -0500

    Update type-signature to match LanguageServer.jl changes

--- eglot-jl.jl
+++ eglot-jl.jl
@@ -2,13 +2,12 @@ import Pkg
 Pkg.activate(@__DIR__)
 
 try
-    @eval using LanguageServer
+    @eval using LanguageServer, SymbolServer
 catch
     @warn "Unable to import LanguageServer. Instantiating project."
     Pkg.instantiate()
-    @eval using LanguageServer
+    @eval using LanguageServer, SymbolServer
 end#try
-using LanguageServer.SymbolServer
 
-server = LanguageServerInstance(stdin, stdout, false, ARGS[1], ARGS[2], Dict())
+server = LanguageServerInstance(stdin, stdout, false, ARGS[1], ARGS[2])
 run(server)

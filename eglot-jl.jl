bd3280846190ef584b60a6397ef56c1aa125f130
Adam Beckmeyer <adam_gpg@thebeckmeyers.xyz>
2020-04-13 12:42:07 -0400

    Update for compatibility with julia 1.4
    Older pinned versions of Julia packages were incompatible with julia
    1.4. Version were bumped and a more robust way of making sure the
    correct packages were found is added (by faffing about with
    LOAD_PATH).

--- eglot-jl.jl
+++ eglot-jl.jl
@@ -1,6 +1,7 @@
-import Pkg
-Pkg.activate(@__DIR__)
-
+# This script must be called with LOAD_PATH set to ["@"] so that the
+# wrong versions of packages aren't picked up. If called with the
+# normal LOAD_PATH, due to the stacked environments, this could pick
+# up a package from "@v#.#".
 try
     @eval using LanguageServer, SymbolServer
 catch
@@ -10,4 +11,7 @@ catch
 end#try
 
 server = LanguageServerInstance(stdin, stdout, false, ARGS[1], ARGS[2])
+# The run command starts additional julia processes which must have a
+# normal LOAD_PATH.
+delete!(ENV, "JULIA_LOAD_PATH")
 run(server)
